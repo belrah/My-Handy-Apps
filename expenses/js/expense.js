218fed2decbd3e1f4e6e7e9334263a046c925482
belrah <belrahj3@gmail.com>
2020-06-25 16:56:07 -0700

    first My App commit

---
+++ expenses/js/expense.js
@@ -0,0 +1,39 @@
+function addMoney(){
+    var totalBudget = parseInt(document.getElementById('totalBudget').innerHTML);
+    var currentBalance = parseInt(document.getElementById('currentBalance').innerHTML);
+    var money = parseInt(document.getElementById('money').value);
+    if (money == " " || isNaN(money)) {
+        alert("enter valid number");
+
+    }else{
+        totalBudget += money;
+        currentBalance += money;
+        document.getElementById('totalBudget').innerHTML = totalBudget;
+        document.getElementById('currentBalance').innerHTML = currentBalance;
+       
+    }
+}
+function addExpenses() {
+    var totalBudget = parseInt(document.getElementById('totalBudget').innerHTML);
+    var currentBalance = parseInt(document.getElementById('currentBalance').innerHTML);
+    var expenseName = document.getElementById('expenseName').value;
+    var expenseAmount = parseInt(document.getElementById('expenseAmount').value);
+
+    if (expenseName == " ") {
+        alert("please enter an expense name")
+    }else if (expenseAmount == " " || isNaN(expenseAmount)) {
+        alert("please enter an expense amount")
+
+    }else{
+        if (expenseAmount <= currentBalance){
+            currentBalance -= expenseAmount;
+            document.getElementById('currentBalance').innerHTML = currentBalance;
+            var para = document.createElement("p");
+            para.innerHTMl = `Expense Name : ${expenseName} <br> Expense Amount : ${expenseAmount}`;
+            document.getElementById('addedExpenses').appendChild(para);
+
+        }else{
+            alert("sorry your expense amount is higher than your current balance")
+        }
+    }
+}
